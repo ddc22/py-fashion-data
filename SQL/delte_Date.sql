1d595fedcee26a1022995f971ba56ed34b25c358
Dehan <dehandecroos@gmail.com>
2019-06-19 18:02:12 +0530

    External files

---
+++ SQL/delte_Date.sql
@@ -0,0 +1,5 @@
+DELETE FROM enactor_trunk.products;
+DELETE FROM enactor_trunk.productmerchandisedetails;
+DELETE FROM enactor_trunk.productprices;
+DELETE FROM enactor_trunk.supplierproducts;
+DELETE FROM enactor_trunk.supplierproductcosts;
